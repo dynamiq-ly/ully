5ea8501bbdf605b3b3948d871293d09fa4e3f52e
Walee Sebiiy <waleesebiiy@gmail.com>
2023-04-20 16:50:53 +0100

    config stitches for next js ssr

--- vercel/pages/_document.tsx
+++ vercel/pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import { getCssText } from '@/config/stitches.config'
+
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <style id='stitches' dangerouslySetInnerHTML={{ __html: getCssText() }} />
+      </Head>
       <body>
         <Main />
         <NextScript />
