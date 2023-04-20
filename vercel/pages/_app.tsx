e73a7a9abb6d9bb9fc4eac5ba64f183e09336e02
Walee Sebiiy <waleesebiiy@gmail.com>
2023-04-20 18:47:56 +0100

    remove the default css styling

--- vercel/pages/_app.tsx
+++ vercel/pages/_app.tsx
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+
+// styles importing
+import { globals } from '@/config/stitches.config'
+globals()
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
